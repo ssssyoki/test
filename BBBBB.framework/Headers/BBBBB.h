01a53e07096f5b075d761ec8a69616034a8d23eb
tianyu <tianyu@qunhemail.com>
2020-07-30 15:36:24 +0800

    feat: a

---
+++ BBBBB.framework/Headers/BBBBB.h
@@ -0,0 +1,19 @@
+//
+//  BBBBB.h
+//  BBBBB
+//
+//  Created by 尤凯 on 2020/7/30.
+//  Copyright © 2020 tianyu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BBBBB.
+FOUNDATION_EXPORT double BBBBBVersionNumber;
+
+//! Project version string for BBBBB.
+FOUNDATION_EXPORT const unsigned char BBBBBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBBBB/PublicHeader.h>
+
+
